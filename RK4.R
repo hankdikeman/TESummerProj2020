ce16487b70f263e6780eab96a70bf41087a0e3c1
hankdikeman <dikem003@umn.edu>
2020-07-05 11:22:07 -0500

    Commit Content: implemented derivative function, changed ReadMe at top of integration
    Commit rationale: wanted derivative function to test integration and optimization with RK4

--- RK4.R
+++ RK4.R
@@ -1,15 +1,17 @@
 #### Function Explanation ####
-#integrates concentration using Runge-Kutta 4 method
-#form of concentration dataframe: 
+# integrates concentration using Runge-Kutta 4 method
+# -----------------------------------
+# form of concentration dataframe: 
 # Col1 = Ester E
 # Col2 = Triglyceride TG
 # Col3 = Diglyceride DG
 # Col4 = Monoglyceride MG
 # Col5 = Alcohol ROH
-# Col6 = Water W
-# Col7 = Glycerol G
-# Col8 = Soap S
-# form of k values
+# Col6 = Glycerol G
+# Col7 = Soap S
+# Col8 = Sodium Hydroxide OH-
+# -----------------------------------
+# form of k values:
 # Col1 = k1
 # Col2 = k1r
 # Col3 = k2
@@ -20,12 +22,13 @@
 # Col8 = k5
 # Col9 = k6
 # Col10 = k7
+# -----------------------------------
 #### RK4 Function ####
 RK4 <- function(k, C0, time, dt, sf) {
-  #determine number of steps and concentration dataframe
+  # determine number of steps and concentration dataframe
   t_steps <- time %/% dt
   C <- C0
-  #integrates using dC derivative function and RK4
+  # integrates using dC derivative function and RK4
   for(i in 2:t_steps) {
     # runs dC function and computes next conc step
     r1 <- dC(k, C[i-1,], sf)
@@ -34,7 +37,7 @@ RK4 <- function(k, C0, time, dt, sf) {
     r4 <- dC(k, C[i-1,] + r3*dt, sf)
     C[i,] <- C[i-1,]+ dt/6 * (r1 + 2*r2 + 2*r3 + r4)
     ##
-    # insert check functions here to check that concentrations match MB equations within error;
+    # insert check functions here to check that concentrations match matl bal equations within error;
     # throw error from this function otherwise
     ##
   }
