fbb4b7c0d92a97b16ca8a072fa125a4920bf9a01
venka146 <venka146@umn.edu>
2020-07-21 23:05:57 -0500

    change in comments

--- gen_graph1.R
+++ gen_graph1.R
@@ -1,4 +1,4 @@
-## Create histogram w/ accumulative prod
+## Create histogram w/ accumulative prod of Ester
 gen_hist <- function(ConcVals){
   graph <- ggplot(data = ConcVals, aes(x = minutes)) +
     geom_histogram(aes(y = Ester/(3*initialvals[1,2]), color = "Ester"))
