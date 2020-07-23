705c387658c6eab836289330d5c180cef77072f0
venka146 <venka146@umn.edu>
2020-07-22 21:08:54 -0500

    Created a visual for table of kvals and arrvals

---
+++ gen_table_vals.R
@@ -0,0 +1,11 @@
+## table of kvals and arrVals for rxn
+gen_table_vals <- function(kvalues, arrVals){
+  kvals <- data.frame(matrix(0, nrow = 1, ncol = 10))
+  colnames(kvals) <- c("k1","k1r","k2","k2r","k3","k3r","k4","k5","k6","k7")
+  ##maybe use cbind 
+  for(i in 1:length(kvals)) {
+    kvals[i,] <- (kvalues[i])
+    kvals
+  }
+  ##return kvals
+}
