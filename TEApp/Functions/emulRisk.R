ca7c3df3dcc85896ee42610c1b06034a7f737677
Patrickgibbs <gibbo143@umn.edu>
2020-08-07 16:38:43 -0400

    edit to emulsion risk function (not done yet)

--- TEApp/Functions/emulRisk.R
+++ TEApp/Functions/emulRisk.R
@@ -1,24 +1,25 @@
-emul_risk <- function(scale_factor, vol, temp) {
-  # Finds changes in concentration between timepoints and normalizes (essentially a crude derivative)
-  added_prod <- transmute(sim_vals,
-            minutes = minutes,
-            prodS_created = (lead(S) - (S))) %>% 
-    filter(!(is.na(prod_created))) %>%
-    mutate(prodS_created = prodS_created / max(abs(prodS_created))) %>%
-    mutate(risk = factor(ifelse(prod_created <= 0.05, "safe", "risky"), levels = c("risky", "safe")))
-    
-    
-    emulRisk <- ggplot(data = added_prod[, ]) +
-      geom_line(
-        mapping = aes(x = minutes, y = (prodS_created), fill = risk),
-        color = "black",
-        show.legend = FALSE
-      ) +
-    scale_fill_manual(values = c("#00ff00", "#ff0000")) +
-    labs(title = "Relative Rates of Ester to Soap",
-         subtitle = paste("Temp = ", temp, "ºC")) +
-    xlab("time (min)") +
-    ylab("Normalized Relative Rate")
-    
-    return (emulRisk)
+emul_risk <- function(sim_df, scale_factor, vol) {
+  print(paste("hello"))
+  # Compute initial mass of all reactants (overall mass will stay constant)
+  initial_mass <- sum(sim_df[1, 2:ncol(sim_df)]*scale_factor*vol*data.frame(matrix(c(300, 885.4, 665, 445, 32.04, 92.09, 250, 39.997), ncol = 8, nrow = 1)))
+  
+  # Create data frame with columns for time points and weight percents of soap 
+  emul_df <- transmute(sim_vals, minutes = minutes, soap_wt_pc = (Soap*scale_factor*vol*250)/initial_mass)
+  
+  print(paste(c(emul_df, "emul")))
+
+  # Create plot of time vs weight percent of soap using ggplot
+  emulRisk <- ggplot(data = emul_df[, ]) +
+    geom_line(
+      mapping = aes(x = minutes, y = (soap_wt_pc), fill = risk),
+      color = "black",
+      show.legend = FALSE
+    ) +
+  scale_fill_manual(values = c("#00ff00", "#ff0000")) +
+  labs(title = "Elmulsification Risk Assessement",
+       subtitle = paste("Temp = ", temp, "ºC")) +
+  xlab("time (min)") +
+  ylab("Weight % of Soap")
+  
+  return (emulRisk)
 }
