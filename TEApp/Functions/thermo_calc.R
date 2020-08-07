2308056c6d39399c0e1df7c5faa8540b24597ed4
venka146 <venka146@umn.edu>
2020-08-07 17:28:12 -0500

    Basic frame work for thermo calc or change in temp per vol

--- TEApp/Functions/thermo_calc.R
+++ TEApp/Functions/thermo_calc.R
@@ -1,6 +1,10 @@
-calcThermo <- function(sim_vals, temp){
-  
-  temp_rate <- 0
+calcThermo <- function(sim_vals, temp, vol, accum_pt){
+  added_prod <-
+    transmute(sim_vals,
+              minutes = minutes)
+  #Need to multiply by something to 
+  accumprod <- filter(sim_vals, minutes >= accum_pt)
+  temp_rate <- accumprod/vol
   
   return (temp_rate)
 }
