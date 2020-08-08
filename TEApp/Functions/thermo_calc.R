e8e0e01aa52929ca38aa598d8d75849961450ac6
venka146 <venka146@umn.edu>
2020-08-07 21:07:25 -0500

    Almost finished thermo_calc. Needs testing

--- TEApp/Functions/thermo_calc.R
+++ TEApp/Functions/thermo_calc.R
@@ -1,9 +1,11 @@
 calcThermo <- function(sim_vals, temp, vol, accum_pt){
+  # Finds changes in concentration between timepoints and normalizes (essentially a crude derivative)
   added_prod <-
     transmute(sim_vals,
               minutes = minutes)
   #Need to multiply by something to 
   accumprod <- filter(sim_vals, minutes >= accum_pt)
+  # Change in temp per vol (mL)
   temp_rate <- accumprod/vol
   
   return (temp_rate)
