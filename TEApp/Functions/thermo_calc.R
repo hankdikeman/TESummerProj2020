4238860608f74bf0165a0268f6d7a8d37e395c6d
venka146 <venka146@umn.edu>
2020-08-07 21:11:03 -0500

    Mostly finished thermo_calc. Needs testing.

--- TEApp/Functions/thermo_calc.R
+++ TEApp/Functions/thermo_calc.R
@@ -1,12 +1,40 @@
-calcThermo <- function(sim_vals, temp, vol, accum_pt){
+## Enthalpies for ester estim. at -37 KJ/mol and soap and -20 KJ/mol
+calcThermo <- function(sim_vals, temp, vol, accum_pt, scale_factor){
+  constant <- scale_factor * vol * 100
   # Finds changes in concentration between timepoints and normalizes (essentially a crude derivative)
-  added_prod <-
+  added_heat <-
     transmute(sim_vals,
-              minutes = minutes)
-  #Need to multiply by something to 
-  accumprod <- filter(sim_vals, minutes >= accum_pt)
-  # Change in temp per vol (mL)
-  temp_rate <- accumprod/vol
+              minutes = minutes,
+              heat = E * constant * (-37) + S * constant * (-20)) %>%
+    filter(!(is.na(heat))) #%>%
+  ##mutate(prod_created = heat / max(abs(heat))) %>%
+    
+  # calculate total accumulated product at given point
+  accum_heat <- filter(sim_vals, minutes >= accum_pt)
+  # round yield number
+  accum_heat <- as.numeric(format(round(accumprod, 2), nsmall = 2))
   
-  return (temp_rate)
+  
+  # Plot rate of change graph
+  heat_rate <- ggplot(data = added_heat[, ]) +
+    geom_area(
+      mapping = aes(x = minutes, y = heat, fill = "green"),
+      color = "black",
+      show.legend = FALSE,
+      alpha = 0.5
+    ) +
+    geom_vline(xintercept = accum_pt, color = "black", size = 1.25) + 
+    geom_text(aes(
+      x = ifelse(accum_pt < max(sim_vals$minutes) / 2, accum_pt + 0.1*max(sim_vals$minutes), accum_pt - 0.1*max(sim_vals$minutes)),
+      y = 0.8,
+      label = paste("Total Heat", accum_heat, "KJ")
+    ), 
+    size = 6) +
+    scale_fill_manual(values = c("#00ff00", "#ff0000")) +
+    labs(title = "Heat Released vs. Time",
+         subtitle = paste("Temp = ", temp, "ºC")) +
+    xlab("time (min)") +
+    ylab("Heat Released (KJ)")
+  
+  return (heat_rate)
 }
